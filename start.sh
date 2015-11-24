b23e4d605562fd3a3993d4f06364131488f45f09
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2015-11-24 18:17:28 +0100

    notebooks

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 FOLDER="$(cd "$(dirname "$0")" && pwd)"
-${FOLDER}/env/bin/ipython notebook #--notebook-dir=${FOLDER}/notebooks --ipython-dir=${FOLDER}/ipy --profile=nbserver
+${FOLDER}/env/bin/ipython notebook
+
