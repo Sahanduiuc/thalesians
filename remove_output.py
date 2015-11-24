6762bc76cf4bb0945b83df677a8c3564db8e6f5a
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2015-11-24 18:09:18 +0100

    remove ouptput of notebooks

---
+++ remove_output.py
@@ -0,0 +1,31 @@
+"""
+Usage: python remove_output.py notebook.ipynb [ > without_output.ipynb ]
+Modified from remove_output by:
+damianavila (https://gist.github.com/damianavila/5305869)
+"""
+import io
+import os
+from nbformat import read, write
+
+
+def remove_outputs(nb):
+    """remove the outputs from a notebook"""
+    for cell in nb.cells:
+        if cell.cell_type == 'code':
+            cell.outputs = []
+            cell.execution_count = None
+
+
+if __name__ == '__main__':
+    #BASEPATH = os.path.dirname(__file__)
+    BASEPATH = os.getcwd()
+    print(BASEPATH)
+    for fname in [f for f in os.listdir(BASEPATH) if f.endswith(".ipynb")]:
+        print(fname)
+        with io.open(fname, 'r') as f:
+            nb = read(f, 4)
+            remove_outputs(nb)
+            print(nb)
+        with io.open(fname, 'w', encoding='utf8') as f:
+            write(nb, f, 4)
+            #print("wrote {0}".format(new_ipynb))
